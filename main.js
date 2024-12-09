c2c9172a83371e77f94f8489c4a34f467548eef8
Earl Murdoch <ehmurdoch47@protonmail.com>
2024-12-09 17:52:49 +0000

    Prototype homepage, not interactive yet

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 let display;
+let NotoSansRegular;
+
+function preload() {
+    NotoSansRegular = loadFont("/assets/NotoSans-Regular.ttf");
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     display = new Display(width, height);
+    textFont(NotoSansRegular);
 }
 
 function draw() {
@@ -23,8 +29,16 @@ class Display {
     #initScreens() {
         let home_screen = new Screen("Home");
         home_screen.addComponent(
-            new Text(this.width / 2, this.height / 2, "Bateria Bot", 140, true
-        ));
+            new Text(this.width/2, this.height/2-200, "Bateria Bot", 140, 
+                true));
+        home_screen.addComponent(new PlayButton(this.width/2-100, 
+            this.height/2-100, 200, 200));
+        home_screen.addComponent(new TextButton(this.width-100, this.height-50,
+            "Credits"));
+        home_screen.addComponent(new TextButton(this.width-250, 50, "Help"));
+        home_screen.addComponent(new TextButton(this.width-100, 50, "Settings"));
+
+        home_screen.addComponent
         this.screens.push(home_screen);
     }
     show() {
@@ -34,9 +48,9 @@ class Display {
 
 
 class Screen {
-    constructor(name) {
+    constructor(name, components=new Array()) {
         this.name = name;
-        this.components = new Array();
+        this.components = components;
         this.mouse_listeners = new Array();
     }
 
@@ -57,6 +71,59 @@ class Component {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+    }
+}
+
+
+class TextButton extends Component {
+    constructor(x, y, msg) {
+        super(x, y);
+        this.msg = msg;
+        this.text_size = 30;
+        this.padding = 10;
+        this.#initComponents();
+    }
+
+    #initComponents() {
+        let bbox = NotoSansRegular.textBounds(this.msg, this.x, this.y,
+            this.text_size);
+        this.rect = [bbox.x-bbox.w/2-this.padding, bbox.y-this.padding, 
+            bbox.w+this.padding*2, bbox.h+this.padding*2];
+        this.text = new Text(this.x, this.y, this.msg, 30);
+    }
+
+    draw() {
+        stroke(0);
+        noFill();
+        rect(...this.rect);
+        this.text.draw();
+    }
+}
+
+
+class PlayButton extends Component {
+    constructor(x, y, w, h) {
+        super(x, y);
+        this.w = w;
+        this.h = h;
+        this.#initComponents();
+    }
+
+    #initComponents() {
+        this.rect = [this.x, this.y, this.w, this.h];
+        this.triangle = [this.x+this.w*1/3, this.y+this.h*1/5, 
+            this.x+this.w*1/3, this.y+this.h*3/5, 
+            this.x+this.w*2/3, this.y+this.h*2/5
+        ];
+        this.text = new Text(this.x+this.w/2, this.y+this.w*4/5, "Play!", 30);
+    }
+
+    draw() {
+        stroke(0);
+        noFill();
+        rect(...this.rect);
+        triangle(...this.triangle);
+        this.text.draw();
     }
 }
 
