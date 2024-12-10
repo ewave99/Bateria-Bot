b4058b1de8c370ba001e332bc4889ac9afb38e6b
Earl Murdoch <ehmurdoch47@protonmail.com>
2024-12-10 15:25:27 +0000

    Add hover functionality to buttons

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ function setup() {
 }
 
 function draw() {
+    cursor(ARROW);
+    display.updateMouseHover(mouseX, mouseY);
     display.show();
 }
 
@@ -31,18 +33,29 @@ class Display {
         home_screen.addComponent(
             new Text(this.width/2, this.height/2-200, "Bateria Bot", 140, 
                 true));
-        home_screen.addComponent(new PlayButton(this.width/2-100, 
-            this.height/2-100, 200, 200));
-        home_screen.addComponent(new TextButton(this.width-100, this.height-50,
-            "Credits"));
-        home_screen.addComponent(new TextButton(this.width-250, 50, "Help"));
-        home_screen.addComponent(new TextButton(this.width-100, 50, "Settings"));
-
-        home_screen.addComponent
+        let play_button = new PlayButton(this.width/2-100, this.height/2-100, 
+            200, 200);
+        home_screen.addComponent(play_button, null,
+            () => play_button.mouseHover());
+        let credits_button = new TextButton(this.width-100, this.height-50,
+            "Credits");
+        home_screen.addComponent(credits_button, null,
+            () => credits_button.mouseHover());
+        let help_button = new TextButton(this.width-250, 50, "Help");
+        home_screen.addComponent(help_button, null,
+            () => help_button.mouseHover());
+        let settings_button = new TextButton(this.width-100, 50, "Settings");
+        home_screen.addComponent(settings_button, null,
+            () => settings_button.mouseHover());
         this.screens.push(home_screen);
     }
+
     show() {
         this.screens[this.current_screen].show();
+    }
+
+    updateMouseHover(mouse_x, mouse_y) {
+        this.screens[this.current_screen].updateMouseHover(mouse_x, mouse_y);
     }
 }
 
@@ -51,7 +64,8 @@ class Screen {
     constructor(name, components=new Array()) {
         this.name = name;
         this.components = components;
-        this.mouse_listeners = new Array();
+        this.mouse_clicks = new Array();
+        this.mouse_hovers = new Array();
     }
 
     show() {
@@ -61,8 +75,27 @@ class Screen {
         }
     }
 
-    addComponent(component) {
+    addComponent(component, mouse_click=null, mouse_hover=null) {
         this.components.push(component);
+        if (mouse_click != null)
+            this.addMouseClick(new MouseListener(...component.getBoundingBox(),
+                mouse_click));
+        if (mouse_hover != null)
+            this.addMouseHover(new MouseListener(...component.getBoundingBox(),
+                mouse_hover));
+    }
+
+    addMouseClick(mouse_click) {
+        this.mouse_clicks.push(mouse_click);
+    }
+
+    addMouseHover(mouse_hover) {
+        this.mouse_hovers.push(mouse_hover);
+    }
+
+    updateMouseHover(mouse_x, mouse_y) {
+        for (let mouse_hover of this.mouse_hovers)
+            mouse_hover.update(mouse_x, mouse_y);
     }
 }
 
@@ -72,6 +105,8 @@ class Component {
         this.x = x;
         this.y = y;
     }
+
+    getBoundingBox() {}
 }
 
 
@@ -81,6 +116,7 @@ class TextButton extends Component {
         this.msg = msg;
         this.text_size = 30;
         this.padding = 10;
+        this.background = 255;
         this.#initComponents();
     }
 
@@ -92,11 +128,22 @@ class TextButton extends Component {
         this.text = new Text(this.x, this.y, this.msg, 30);
     }
 
+    getBoundingBox() {
+        return this.rect;
+    }
+
+    mouseHover() {
+        this.background = 180;
+        cursor(HAND);
+    }
+
     draw() {
         stroke(0);
-        noFill();
+        fill(this.background);
         rect(...this.rect);
         this.text.draw();
+        //reset background
+        this.background = 255;
     }
 }
 
@@ -106,6 +153,7 @@ class PlayButton extends Component {
         super(x, y);
         this.w = w;
         this.h = h;
+        this.background = 255;
         this.#initComponents();
     }
 
@@ -118,12 +166,23 @@ class PlayButton extends Component {
         this.text = new Text(this.x+this.w/2, this.y+this.w*4/5, "Play!", 30);
     }
 
+    mouseHover() {
+        this.background = 180;
+        cursor(HAND);
+    }
+
+    getBoundingBox() {
+        return this.rect;
+    }
+
     draw() {
         stroke(0);
-        noFill();
+        fill(this.background);
         rect(...this.rect);
         triangle(...this.triangle);
         this.text.draw();
+        //reset background
+        this.background = 255;
     }
 }
 
@@ -135,6 +194,12 @@ class Text extends Component {
         this.size = size;
         this.bold = bold;
         this.align = align;
+    }
+
+    getBoundingBox() {
+        let bbox = NotoSansRegular.textBounds(this.msg, this.x, this.y,
+            this.text_size);
+        return [bbox.x, bbox.y, bbox.w, bbox.h];
     }
 
     draw() {
@@ -157,6 +222,10 @@ class Image extends Component {
         this.h = h;
         this.src = src;
     }
+
+    getBoundingBox() {
+        return [this.x, this.y, this.w, this.h];
+    }
 }
 
 
@@ -167,18 +236,33 @@ class Checkbox extends Component {
         this.checked = checked;
     }
 
+    getBoundingBox() {
+        return [this.x, this.y, this.size, this.size];
+    }
+
     toggle() {}
 }
 
 
 class MouseListener {
-    constructor(x0, y0, x1, y1, callback) {
-        this.x0 = x0;
-        this.y0 = y0;
-        this.x1 = x1;
-        this.y1 = y1;
+    constructor(x, y, w, h, callback) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
         this.callback = callback;
     }
 
-    coordsInBox(x, y) {}
+    update(mouse_x, mouse_y) {
+        if (this.coordsInBox(mouse_x, mouse_y))
+            this.callback();
+    }
+
+    coordsInBox(x, y) {
+        if (x < this.x) return false;
+        if (x > this.x + this.w) return false;
+        if (y < this.y) return false;
+        if (y > this.y + this.h) return false;
+        return true;
+    }
 }
